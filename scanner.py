b079fa740b2e0548f0cfec08efbee36ccfeb70c4
Jamiu-Tijani <jimniz01@gmail.com>
2024-01-20 10:08:36 -0500

    add arguments

--- scanner.py
+++ scanner.py
@@ -1,40 +1,48 @@
 import sys
 import socket
 from datetime import datetime
+import argparse
 
-#Define our target
+# Setup Argument Parser
+parser = argparse.ArgumentParser(description="Jay Port Scanner 1.0")
+parser.add_argument("IP", help="IP address of the target to scan")
+parser.add_argument("--start_port", type=int, default=0, help="Start of the port range to scan (default: 0)")
+parser.add_argument("--end_port", type=int, default=9000, help="End of the port range to scan (default: 9000)")
+parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")
+
+# Parse Arguments
+args = parser.parse_args()
+
+# Define our target
+target = socket.gethostbyname(args.IP)
+
+# Print Welcome Banner
 print("_"*50)
 print("WELCOME TO JAY PORT SCANNER 1.0")
 print("_"*50)
-
 print("")
 
-IP = input("Please input IP")
-if  len(IP) > 5:
-        target = socket.gethostbyname(IP)
-else:
-        print("Please Check your ip if it's correct.")
-        print("")
-        sys.exit()
-    #add a pretty banner
+# Add a pretty banner
 print("-"*50)
-print("Scanning Target" + target)
-print("time started" +str(datetime.now()))
+print(f"Scanning Target: {target}")
+print("Time started: " + str(datetime.now()))
 print("-"*50)
+
+# Scanning
 try:
-        for port in range(0,9000):
-                s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-                socket.setdefaulttimeout(1)
-                result = s.connect_ex((target,port)) #returns error indicator
-                if result == 0:
-                        print('port {} is open'.format(port))
-                
-                s.close()
+    for port in range(args.start_port, args.end_port + 1):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target, port))  # returns error indicator
+        if result == 0 and args.verbose:  # Only print if verbose is enabled
+            print(f'Port {port} is open')
+        s.close()
+
 except KeyboardInterrupt:
-        print("\nExiting program.")
-        sys.exit()
+    print("\nExiting program.")
+    sys.exit()
 except socket.gaierror:
-        print("Hostname could not be resolved.")
-        sys.exit()
+    print("Hostname could not be resolved.")
+    sys.exit()
 except socket.error:
-        print("Could not connect to server")        
+    print("Could not connect to server")
